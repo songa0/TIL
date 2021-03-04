7f35f2e088c85daf254eee709995ff099e867af6
Songa0 <dolphin2440@naver.com>
2021-03-04 22:00:50 +0900

    Add filter

--- Clone Coding/Dream Coding Mini game/src/main.js
+++ Clone Coding/Dream Coding Mini game/src/main.js
@@ -8,21 +8,42 @@ function loadItems(){
 loadItems()
  .then(items =>{
      displayItems(items);
+     setEventListeners(items);
  })
  .catch(console.log);
 
 
  function displayItems(items){
      const itemList = document.querySelector("#itemList");
-    for(var item of items){   
-        var li = document.createElement('li');
-        var img = document.createElement('img');
-        img.src = item.image;
-        var txt = document.createTextNode(`${item.gender}, ${item.size}`);
-        li.appendChild(img);
-        li.appendChild(txt);
-        itemList.appendChild(li);
-    }
-        
+     
+     var listItem = '' ;
+     for(var item of items){   
+        listItem += `<li class = "${item.type} ${item.color}"><img src = ${item.image} >${item.gender}, ${item.size}</li>`;
+     }
+     itemList.innerHTML = listItem;
+     
+ }
+
+ function setEventListeners(items){
+    const header = document.querySelector('header');
+    header.addEventListener('click', () => displayItems(items));
     
+    
+    const btnList = document.querySelector('nav ul');
+    btnList.addEventListener('click',event =>clickEvent(event, document.querySelectorAll('section ul li')));
+
+ }
+
+ function clickEvent(event, items){
+     const key = event.target.dataset.key;
+     const value = event.target.dataset.value;
+     
+     for(var item of items){
+        if(item.classList.contains(value)){
+            item.classList.remove('invisible');
+        }else{
+            item.classList.add('invisible');
+        }
+     }
+     
  }
