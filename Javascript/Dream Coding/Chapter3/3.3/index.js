8a4ce694c8c11fc0505e0d352b7e6dc4e91cc5f4
Songa0 <dolphin2440@naver.com>
2021-04-01 23:38:18 +0900

    좌표 개선 : painting 만 일어나도록

--- Javascript/Dream Coding/Chapter3/3.3/index.js
+++ Javascript/Dream Coding/Chapter3/3.3/index.js
@@ -3,18 +3,30 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', event=>{
-    const x = event.clientX;
-    const y = event.clientY;
+addEventListener('load', ()=>{
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width/2;
+    const targetHalfHeight = targetRect.height/2;
     
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
+    document.addEventListener('mousemove', event=>{
+        const x = event.clientX;
+        const y = event.clientY;
+        
+        //layout 발생하지 않도록 수정. painting 만 발생하도록 함
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        //vertical.style.left = `${x}px`;
+        //horizontal.style.top = `${y}px`;
+        
+        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
     
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-
-    tag.innerHTML = `${x}px, ${y}px`;
+        //target.style.left = `${x}px`;
+        //target.style.top = `${y}px`;
+        tag.style.transform = `translate(${x+20}px, ${y+20}px)`;
+        // tag.style.top = `${y}px`;
+        // tag.style.left = `${x}px`;
+    
+        tag.innerHTML = `${x}px, ${y}px`;
+    });
+    
 });
