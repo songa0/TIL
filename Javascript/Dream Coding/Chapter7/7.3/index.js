faa939f761ccac45c08f97528ffbc2c255807392
Songa0 <dolphin2440@naver.com>
2021-04-23 23:31:29 +0900

    add start/stop btn func

--- Javascript/Dream Coding/Chapter7/7.3/index.js
+++ Javascript/Dream Coding/Chapter7/7.3/index.js
@@ -1,6 +1,8 @@
 const timer = document.querySelector('#timer');
 const randomNum  = Math.floor(Math.random() * 10); 
 const carrotCnt = document.querySelector('#carrotCnt');
+const startStopBtn = document.querySelector('#startStopBtn');
+let startStopBtnFlag = true; //start
 let timerSec = randomNum;
 let leftCarrotNum = randomNum;
 let intervalFunc;
@@ -14,7 +16,10 @@ function startClock(){
         timerSec--;
         timer.innerHTML = `0:${timerSec}`;
 
-        if(timerSec<=0) stopClock();
+        if(timerSec<=0) {
+            
+            stopClock();
+        }
     }, 1000);
 
     
@@ -24,7 +29,21 @@ function stopClock(){
     clearInterval(intervalFunc);
 }
 
-startClock();
+//startClock();
+
+
+startStopBtn.addEventListener('click',function(){
+    if(startStopBtnFlag){
+        startStopBtn.innerHTML = '<i class="fas fa-stop"></i>';
+        startStopBtnFlag = false;
+        startClock();
+    }else{
+        startStopBtn.innerHTML = '<i class="fas fa-play"></i>';
+        startStopBtnFlag = true;
+        stopClock();
+        
+    }
+});
 
 
 
