2f67cc5b7251d3d36ffa7eba909197c4e70caf8c
Songa0 <dolphin2440@naver.com>
2021-05-08 23:19:16 +0900

    모듈화

--- Javascript/Dream Coding/Chapter7/7.3/src/game.js
+++ Javascript/Dream Coding/Chapter7/7.3/src/game.js
@@ -1,29 +1,58 @@
 'use strict';
 
+import Field from './field.js';
+import * as sound from './sound.js';
+import PopUp from './popup.js';
+
 export default class Game{
-    constructor(){
+    constructor(randomNum){
         this.timer = document.querySelector('#timer');
         this.carrotCnt = document.querySelector('#carrotCnt');
         this.startStopBtn = document.querySelector('#startStopBtn');
         this.header = document.querySelector('header');
 
+        this.timer.innerHTML = '0:0';
+        this.carrotCnt.innerHTML = 0;
+
         this.startStopBtnFlag; //start
         this.timerSec = 0;
         this.intervalFunc;
-        this.randomNum = 0;
+        this.randomNum = randomNum;
         this.startStopBtn.addEventListener('click',(e)=>this.onBtnClick(e));
 
+        this.gameFieldSection = new Field(0,0);
+        this.gameFieldSection.setClickListener(this.onItemClick);
+        this.setStopTimeListner(this.stop);
+        this.setClickListener(this.gameStartStop);
+
+        this.finishGameBanner = new PopUp();
+        this.finishGameBanner.setClickListener(()=>{
+            this.start();
+        });
+        
     }
 
     startClock(){ //카운트 다운 시작
         this.intervalFunc = setInterval(() => {
             this.timer.innerHTML = `0:${--this.timerSec}`;
             if(this.timerSec<=0) {  
-                //stopgame
                 this.stopTimeFunc&&this.stopTimeFunc();
             }
         }, 1000);
     }
+
+    onItemClick = (item) =>{
+        if(item==='bug'){
+         sound.playBug();
+         this.finish();
+        }else if(item === 'carrot' ){ //carrot
+             this.carrotCnt.innerHTML = this.gameFieldSection.leftCarrotNum;
+             sound.playCarrot();
+             if(this.gameFieldSection.leftCarrotNum==0){
+                this.finish();
+             }
+        }
+     };
     
     stopClock(){//카운트 다운 멈춤
         clearInterval(this.intervalFunc);
@@ -49,4 +78,51 @@ export default class Game{
         }
     }
 
+    start(){ //게임 시작
+        this.init(); //초기화
+        this.startClock(); //시간초 
+        this.startStopBtnFlag = true;
+        this.setStartStopBtn(this.startStopBtnFlag); //정지 버튼으로 바꾸기
+        this.gameFieldSection.init() //벌레, 당근 뿌리기
+        this.finishGameBanner.showPopup(false);
+        sound.playBg();
+    }
+    
+    stop(){
+        this.stopClock(); //시간 멈추기
+        this.finishGameBanner.showPopup(true,'REPLAY?');
+        sound.stopBg();
+    }
+    
+    finish(){ //게임 종료
+        this.stopClock(); //시간 멈추기
+        this.startStopBtnFlag = false;
+        this.setStartStopBtn(this.startStopBtnFlag); //시작 버튼으로 바꾸기
+        sound.stopBg();
+        if(this.gameFieldSection.leftCarrotNum>0){ //게임 종료되었는데 남은 당근이 있을 시에 YOU LOST DIV 보여주기
+            this.finishGameBanner.showPopup(true,'YOU LOSE');    
+        }else {
+            sound.playWin();
+            this.finishGameBanner.showPopup(true,'YOU WIN');
+        }
+    }
+
+    gameStartStop =(flag) =>{
+        if(flag==='stop'){
+            this.stop();
+        }else if(flag==='start'){
+            this.start();
+        }
+    }
+    
+    
+    init(){ //세팅
+        this.timerSec = this.randomNum;
+        this.timerSec = this.randomNum;
+        this.gameFieldSection.leftCarrotNum = this.randomNum;
+        this.gameFieldSection.bugNum = this.randomNum;
+        this.timer.innerHTML = `0:${this.timerSec}`;
+        this.carrotCnt.innerHTML = this.gameFieldSection.leftCarrotNum;
+        this.gameFieldSection.gameField.innerHTML = '';     
+    }
 }
--- Javascript/Dream Coding/Chapter7/7.3/src/index.js
+++ Javascript/Dream Coding/Chapter7/7.3/src/index.js
@@ -1,84 +1,14 @@
 'use strict';   
-import PopUp from './popup.js';
-import Field from './field.js';
-import * as sound from './sound.js';
 import Game from './game.js';
+import PopUp from './popup.js';
 
-const gameHeader = new Game();
-const finishGameBanner = new PopUp();
-const gameFieldSection = new Field(0,0);
+const gameHeader = new Game( Math.floor(Math.random() * 10+1));
 
-timer.innerHTML = '0:0';
-gameHeader.carrotCnt.innerHTML = 0;
-
+finishGameBanner = new PopUp();
 
 finishGameBanner.setClickListener(()=>{
-    gameStart();
+    game.start();
 });
 
-gameFieldSection.setClickListener(onItemClick);
-
-gameHeader.setClickListener(gameStartStop);
-gameHeader.setStopTimeListner(gameStop);
-
-function onItemClick(item){
-   if(item==='bug'){
-    sound.playBug();
-     gameOver();
-   }else if(item === 'carrot' ){ //carrot
-        gameHeader.carrotCnt.innerHTML = gameFieldSection.leftCarrotNum;
-        sound.playCarrot();
-        if(gameFieldSection.leftCarrotNum==0){
-            gameOver();
-        }
-   }
-};
-
-function gameStartStop(flag){
-    if(flag==='stop'){
-        gameStop();
-    }else if(flag==='start'){
-        gameStart();
-    }
-}
-function init(){ //세팅
-    gameHeader.randomNum  = Math.floor(Math.random() * 10+1);
-    gameHeader.timerSec = gameHeader.randomNum;
-    gameHeader.timerSec = gameHeader.randomNum;
-    gameFieldSection.leftCarrotNum = gameHeader.randomNum;
-    gameFieldSection.bugNum = gameHeader.randomNum;
-    gameHeader.timer.innerHTML = `0:${gameHeader.timerSec}`;
-    carrotCnt.innerHTML = gameFieldSection.leftCarrotNum;
-    gameFieldSection.gameField.innerHTML = '';     
-}
-
-function gameOver(flag){ //게임 종료
-    gameHeader.stopClock(); //시간 멈추기
-    gameHeader.startStopBtnFlag = false;
-    gameHeader.setStartStopBtn(gameHeader.startStopBtnFlag); //시작 버튼으로 바꾸기
-    sound.stopBg();
-    if(gameFieldSection.leftCarrotNum>0){ //게임 종료되었는데 남은 당근이 있을 시에 YOU LOST DIV 보여주기
-        finishGameBanner.showPopup(true,'YOU LOSE');    
-    }else {
-        sound.playWin();
-        finishGameBanner.showPopup(true,'YOU WIN');
-    }
-}
-
-function gameStart(){ //게임 시작
-    init(); //초기화
-    gameHeader.startClock(); //시간초 
-    gameHeader.startStopBtnFlag = true;
-    gameHeader.setStartStopBtn(gameHeader.startStopBtnFlag); //정지 버튼으로 바꾸기
-    gameFieldSection.init() //벌레, 당근 뿌리기
-    finishGameBanner.showPopup(false);
-    sound.playBg();
-}
-
-function gameStop(){
-    gameHeader.stopClock(); //시간 멈추기
-    finishGameBanner.showPopup(true,'REPLAY?');
-    sound.stopBg();
-}
 
 
