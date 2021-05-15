344a4ddd5b7a166ae9651554a43fc52990b56e61
Songa0 <dolphin2440@naver.com>
2021-05-15 22:59:48 +0900

    Fix func

--- React/Chapter4/habit-tracker/src/app.jsx
+++ React/Chapter4/habit-tracker/src/app.jsx
@@ -4,16 +4,28 @@ import Habits from './components/habits';
 
 class App extends Component{
   state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Eating', count: 0 },           
+    ]
 
   };
 
+  handleClickHabit() {
+    
+  }
   render() {
     return (
       
       <>
-        <header className="habit-header">Habit Tracker</header>
+        <header className="habit-header">
+          <i className="fas fa-leaf"></i> 
+          <span className='habit-title'>Habit Tracker</span>
+          <span className='habit-total-count'>0</span>
+        </header>
         <input type="text" placeholder="Habit" className="habit-input"/><button className="habit-add">Add</button>
-        <Habits />
+        <Habits habits={this.state.habits} onClick={ this.handleClickHabit}/>
         <button className='habit-reset'>Reset All</button>
         
         </>
--- React/Chapter4/habit-tracker/src/components/habits.jsx
+++ React/Chapter4/habit-tracker/src/components/habits.jsx
@@ -3,36 +3,32 @@ import Habit from './habit';
 
 class Habits extends Component {
     state = {
-        habits: [
-            {id:1, name: 'Reading', count: 0 },
-            {id:2, name: 'Running', count: 0 },
-            {id:3, name: 'Eating', count: 0 },
-            
-        ]
+        
     };
     handleIncrement = (habit) => {
-        const habits = [...this.state.habits];
+        const habits = [...this.props.habits];
         const index = habits.indexOf(habit);
         habits[index].count++;
         this.setState({ habits: habits });
 
     }
     handleDecrement = (habit) => {
-       const habits = [...this.state.habits];
+        const habits = [...this.props.habits];
         const index = habits.indexOf(habit);
         const count = habits[index].count - 1;
         habits[index].count = count < 0 ? 0 : count;
         this.setState({ habits: habits });
     }
     handleDelete = (habit) => {
-        const habits = this.state.habits.filter(item => item.id !== habit.id);
+        const habits = this.props.habits.filter(item => item.id !== habit.id);
         this.setState({ habits: habits });
     }
-    render() {  
+    render() {
+        const habits = this.props.habits;
         return (
             <ul>
                 {
-                    this.state.habits.map(item => ( 
+                    habits.map(item => (
                         <Habit
                             key={item.id}
                             habit={item}
